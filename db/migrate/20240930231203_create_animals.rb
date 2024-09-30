1ec00b5c6776fb5027b1c9337571c6165c5e70c4
Ellen Uttley <ellenuttley@gmail.com>
2024-10-01 00:14:17 +0100

    animal table migration

--- db/migrate/20240930231203_create_animals.rb
+++ db/migrate/20240930231203_create_animals.rb
@@ -1,10 +1,10 @@
 class CreateAnimals < ActiveRecord::Migration[7.1]
   def change
     create_table :animals do |t|
-      t.string :name
-      t.boolean :dinosaur
-      t.boolean :marine
-      t.boolean :researched
+      t.string :name, null: false
+      t.boolean :dinosaur, default: false
+      t.boolean :marine, default: false
+      t.boolean :researched, default: false
 
       t.timestamps
     end
