24563b7cbee0f90ae0e02327c98bae3d862cb087
Ellen Uttley <ellenuttley@gmail.com>
2024-10-01 12:27:11 +0100

    added test - adding valid research animal. also updated the language of the fail messages

--- test/models/animal_test.rb
+++ test/models/animal_test.rb
@@ -3,12 +3,19 @@ require "test_helper"
 class AnimalTest < ActiveSupport::TestCase
   test "should not save animal without name" do
     animal = Animal.new
-    assert_not animal.save, "FAIL : Saved the animal without a name"
+    assert_not animal.save, "FAIL : Animal saved without a name"
   end
 
   test "should not save animal with duplicate name" do
     Animal.create(name: "Giraffe")
     animal = Animal.new(name: "Giraffe")
-    assert_not animal.save, "FAIL : Saved the animal with a duplicate name"
+    assert_not animal.save, "FAIL : Animal saved with duplicate name"
   end
+
+  test "should save valid researched animal" do
+    animal = Animal.new(name: "Okapi", dinosaur: false, marine: false, researched: true)
+    assert animal.save, "FAIL : Could not save researched animal"
+  end
+
+
 end
