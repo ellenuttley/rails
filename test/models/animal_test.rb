24809408da70e6c53dcba79ce6c8822cd0d1ae1b
Ellen Uttley <ellenuttley@gmail.com>
2024-10-01 12:12:48 +0100

    added test - assert cannot save animal with duplicate name

--- test/models/animal_test.rb
+++ test/models/animal_test.rb
@@ -5,4 +5,10 @@ class AnimalTest < ActiveSupport::TestCase
     animal = Animal.new
     assert_not animal.save, "FAIL : Saved the animal without a name"
   end
+
+  test "should not save animal with duplicate name" do
+    Animal.create(name: "Giraffe")
+    animal = Animal.new(name: "Giraffe")
+    assert_not animal.save, "FAIL : Saved the animal with a duplicate name"
+  end
 end
