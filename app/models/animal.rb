35c56099a887046f01e37ac4b3ca73f9c75ea4ac
Ellen Uttley <ellenuttley@gmail.com>
2024-10-02 23:22:16 +0100

    added before_save callback and private method, to change any animal name to title case before it is added to the database. test now passing

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,13 @@
 class Animal < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  before_save :capitalize_name
 
   scope :dinosaurs, -> { where(dinosaur: true) }
   scope :marine, -> { where(marine: true) }
   scope :researched, -> { where(researched: true) }
+
+  private
+  def capitalize_name
+    self.name = name.titleize if name.present?
+  end
 end
