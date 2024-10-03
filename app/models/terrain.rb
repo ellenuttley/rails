a87a76a4919e32ca4783a0067a4adca4516b8056
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 23:33:57 +0100

    added validation, test now passing

--- app/models/terrain.rb
+++ app/models/terrain.rb
@@ -3,4 +3,5 @@ class Terrain < ApplicationRecord
   has_many :animals, through: :animal_terrains
 
   validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
