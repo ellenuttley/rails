730750bf2f65e4b8e7bf045692a48b1f18020b8a
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 23:20:15 +0100

    edited tests to properly use factory - both passing

--- test/models/terrain_test.rb
+++ test/models/terrain_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class TerrainTest < ActiveSupport::TestCase
   test "valid terrain" do
-    terrain = create(:terrain, name: "Grass", price: 40)
+    terrain = build(:terrain)
     assert terrain.valid?
   end
 
-  test "should not save terrain without name" do
-    animal = Terrain.new
-    assert_not animal.save, "FAIL : Terrain saved without a name"
+  test "terrain must have name" do
+    terrain = build(:terrain, name: nil)
+    assert_not terrain.valid?
+    assert_includes terrain.errors[:name], "can't be blank"
   end
 end
