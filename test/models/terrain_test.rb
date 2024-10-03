d7247e0c0103c7c5ec63a5aa6c98a3b4574782a2
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 22:29:02 +0100

    added test - terrain must have a name. Currently failing, as expected

--- test/models/terrain_test.rb
+++ test/models/terrain_test.rb
@@ -5,4 +5,10 @@ class TerrainTest < ActiveSupport::TestCase
     terrain = Terrain.new(name: "Grass", price: 40)
     assert terrain.valid?
   end
+
+  test "terrain must have name" do
+    terrain = Terrain.new(price: 100)
+    assert_not terrain.valid?
+    assert_includes terrain.errors[:name], "FAIL : Terrain must have a name"
+  end
 end
