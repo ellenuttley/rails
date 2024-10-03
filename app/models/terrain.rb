8ddc2828f5df4aa308f1536f76e692aa0b64a008
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 23:12:32 +0100

    added validation, and edited test - test now passing

--- app/models/terrain.rb
+++ app/models/terrain.rb
@@ -1,4 +1,6 @@
 class Terrain < ApplicationRecord
   has_many :animal_terrains
   has_many :animals, through: :animal_terrains
+
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/terrain_test.rb
+++ test/models/terrain_test.rb
@@ -2,13 +2,12 @@ require "test_helper"
 
 class TerrainTest < ActiveSupport::TestCase
   test "valid terrain" do
-    terrain = Terrain.new(name: "Grass", price: 40)
+    terrain = create(:terrain, name: "Grass", price: 40)
     assert terrain.valid?
   end
 
-  test "terrain must have name" do
-    terrain = Terrain.new(price: 100)
-    assert_not terrain.valid?
-    assert_includes terrain.errors[:name], "FAIL : Terrain must have a name"
+  test "should not save terrain without name" do
+    animal = Terrain.new
+    assert_not animal.save, "FAIL : Terrain saved without a name"
   end
 end
