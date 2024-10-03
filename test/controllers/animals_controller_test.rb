4967a45f80bbd44379a0fa69a31a099565da1dcb
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 20:14:43 +0100

    added boilerplate view. test to check route is passing

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 class AnimalsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get animals_index_url
+    get root_url
     assert_response :success
-    assert_not_nil assigns(:animals)
+    assert_not_nil @controller.instance_variable_get('@animals')
   end
+
 end
