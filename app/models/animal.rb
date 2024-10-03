d90a1d2fbe134265984ed416f4e2d723b632ad3f
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 13:54:16 +0100

    added validation and method to animal model, test now passing

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,7 @@
 class Animal < ApplicationRecord
+  validate :cannot_be_marine_and_dinosaur
   validates :name, presence: true, uniqueness: true
+
   before_save :capitalize_name
 
   scope :dinosaurs, -> { where(dinosaur: true) }
@@ -9,5 +11,11 @@ class Animal < ApplicationRecord
   private
   def capitalize_name
     self.name = name.titleize if name.present?
+  end
+
+  def cannot_be_marine_and_dinosaur
+    if marine && dinosaur
+      errors.add(:base, "Cannot be both marine and dinosaur")
+    end
   end
 end
