67901e769b6b96ed0ff0459c4fdb3fc95f3e0839
Ellen Uttley <ellenuttley@gmail.com>
2024-10-03 23:54:17 +0100

    added initial integration test - create valid terrain using factory. test is passing

--- test/integration/terrain_integration_test.rb
+++ test/integration/terrain_integration_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class TerrainIntegrationTest < ActionDispatch::IntegrationTest
+    test "terrain and animal associations work correctly" do
+        # Create a terrain using factory
+        terrain = create(:terrain)
+        assert terrain.valid?
+        assert_equal 0, terrain.animals.count
+        end
   
 end
