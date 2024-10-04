ff23a4100b0734ea3052802109698d976fed2541
Ellen Uttley <ellenuttley@gmail.com>
2024-10-04 11:56:04 +0100

    added validation to the animal terrain model. test now passing

--- app/models/animal_terrain.rb
+++ app/models/animal_terrain.rb
@@ -1,4 +1,5 @@
 class AnimalTerrain < ApplicationRecord
   belongs_to :animal
   belongs_to :terrain
+  validates :percentage, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
