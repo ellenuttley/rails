ea325991c8c94f5a5dc6117919b7495182e7f33b
Ellen Uttley <ellenuttley@gmail.com>
2024-10-05 10:52:06 +0100

    all animal terrains now in database. showing on frontend ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ animals_to_add = [
   { name: "Herrerasaurus", dinosaur: true, marine: false, researched: false },
   { name: "Plateosaurus", dinosaur: true, marine: false, researched: true },
   { name: "Allosaurus", dinosaur: true, marine: false, researched: false },
-  { name: "Pacific Walrus", dinosaur: false, marine: true, researched: false },
+  { name: "Pacific Walrus", dinosaur: false, marine: false, researched: false },
   { name: "Shortfin Mako Shark", dinosaur: false, marine: true, researched: true },
   { name: "Southern Sea Otter", dinosaur: false, marine: true, researched: false },
   { name: "Sperm Whale", dinosaur: false, marine: true, researched: false },
@@ -66,7 +66,7 @@ animals_to_add = [
   { name: "Velociraptor", dinosaur: true, marine: false, researched: true },
   { name: "Giant Ground Sloth", dinosaur: true, marine: false, researched: false },
   { name: "Giant Tortoise", dinosaur: true, marine: false, researched: false },
-  { name: "Saber-toothed Cat", dinosaur: true, marine: false, researched: false },
+  { name: "Saber Toothed Cat", dinosaur: true, marine: false, researched: false },
   { name: "Woolly Mammoth", dinosaur: true, marine: false, researched: true },
   { name: "Asian Black Bear", dinosaur: false, marine: false, researched: false },
   { name: "Giant Panda", dinosaur: false, marine: false, researched: true },
@@ -108,7 +108,6 @@ animals_to_add = [
   { name: "Red Kangaroo", dinosaur: false, marine: false, researched: false },
   { name: "Spotted Hyena", dinosaur: false, marine: false, researched: false },
   { name: "Thomson's Gazelle", dinosaur: false, marine: false, researched: false },
-  { name: "Triceratops", dinosaur: true, marine: false, researched: false },
   { name: "Bongo", dinosaur: false, marine: false, researched: false },
   { name: "Chimpanzee", dinosaur: false, marine: false, researched: false },
   { name: "Clouded Leopard", dinosaur: false, marine: false, researched: false },
@@ -229,7 +228,619 @@ animals_with_terrains = [
       { name: "Grey Stone", percentage: 20 },
       { name: "Fresh Water", percentage: 5 }
     ]
-  }
+  },
+  {
+    name: "Bongo",
+    terrains: [
+      { name: "Grass", percentage: 20 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Rainforest Floor", percentage: 70 }
+    ]
+  },
+  {
+    name: "Chimpanzee",
+    terrains: [
+      { name: "Grass", percentage: 10 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 85 }
+    ]
+  },
+  {
+    name: "Clouded Leopard",
+    terrains: [
+      { name: "Grass", percentage: 20 },
+      { name: "Rainforest Floor", percentage: 80 }
+    ]
+  },
+  {
+    name: "Giant Anteater",
+    terrains: [
+      { name: "Grass", percentage: 8 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Rainforest Floor", percentage: 80 },
+      { name: "Fresh Water", percentage: 2 }
+    ]
+  },
+  {
+    name: "Jaguar",
+    terrains: [
+      { name: "Dirt", percentage: 10 },
+      { name: "Rainforest Floor", percentage: 75 },
+      { name: "Fresh Water", percentage: 15 }
+    ]
+  },
+  {
+    name: "Lowland Gorilla",
+    terrains: [
+      { name: "Grass", percentage: 20 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Rainforest Floor", percentage: 70 }
+    ]
+  },
+  {
+    name: "Mandrill",
+    terrains: [
+      { name: "Grass", percentage: 10 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 85 }
+    ]
+  },
+  {
+    name: "Okapi",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 88 },
+      { name: "Fresh Water", percentage: 2 }
+    ]
+  },
+  {
+    name: "White Bengal Tiger",
+    terrains: [
+      { name: "Grass", percentage: 15 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 70 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Lambeosaurus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 85 },
+      { name: "Grass", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Spinosaurus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 65 },
+      { name: "Grass", percentage: 25 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Styracosaurus",
+    terrains: [
+      { name: "Deciduous Floor", percentage: 70 },
+      { name: "Fresh Water", percentage: 10 },
+      { name: "Grass", percentage: 5 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 5 }
+    ]
+  },
+  {
+    name: "Tyrannosaurus Rex",
+    terrains: [
+      { name: "Grass", percentage: 60 },
+      { name: "Grey Stone", percentage: 25 },
+      { name: "Deciduous Floor", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Velociraptor",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 85 },
+      { name: "Grass", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Giant Ground Sloth",
+    terrains: [
+      { name: "Grass", percentage: 50 },
+      { name: "Snow", percentage: 30 },
+      { name: "Brown Stone", percentage: 15 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Giant Tortoise",
+    terrains: [
+      { name: "Fresh Water", percentage: 60 },
+      { name: "Snow", percentage: 30 },
+      { name: "Grey Stone", percentage: 10 }
+    ]
+  },
+  {
+    name: "Saber Toothed Cat",
+    terrains: [
+      { name: "Grey Stone", percentage: 80 },
+      { name: "Snow", percentage: 10 },
+      { name: "Brown Stone", percentage: 5 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Woolly Mammoth",
+    terrains: [
+      { name: "Snow", percentage: 60 },
+      { name: "Grey Stone", percentage: 20 },
+      { name: "Brown Stone", percentage: 15 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+    {
+    name: "Pacific Walrus",
+    terrains: [
+      { name: "Salt Water", percentage: 60 },
+      { name: "Snow", percentage: 30 },
+      { name: "Grey Stone", percentage: 10 }
+    ]
+  },
+  {
+    name: "Southern Sea Otter",
+    terrains: [
+      { name: "Salt Water", percentage: 90 },
+      { name: "Grey Stone", percentage: 10 }
+    ]
+  },
+  {
+    name: "California Sea Lion",
+    terrains: [
+      { name: "Salt Water", percentage: 90 },
+      { name: "Grey Stone", percentage: 10 }
+    ]
+  },
+  {
+    name: "Elephant Seal",
+    terrains: [
+      { name: "Salt Water", percentage: 75 },
+      { name: "Grey Stone", percentage: 15 },
+      { name: "Snow", percentage: 10 }
+    ]
+  },
+  {
+    name: "Woolly Rhinoceros",
+    terrains: [
+      { name: "Snow", percentage: 75 },
+      { name: "Grey Stone", percentage: 15 },
+      { name: "Brown Stone", percentage: 10 }
+    ]
+  },
+   {
+    name: "Apatosaurus",
+    terrains: [
+      { name: "Deciduous Floor", percentage: 75 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 10 },
+      { name: "Grass", percentage: 5 }
+    ]
+  },
+  {
+    name: "Camptosaurus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 80 },
+      { name: "Grass", percentage: 10 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Caudipteryx",
+    terrains: [
+      { name: "Rainforest Floor", percentage: 50 },
+      { name: "Fresh Water", percentage: 30 },
+      { name: "Grass", percentage: 20 }
+    ]
+  },
+  {
+    name: "Kentrosaurus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 70 },
+      { name: "Fresh Water", percentage: 15 },
+      { name: "Grass", percentage: 10 },
+      { name: "Dirt", percentage: 5 }
+    ]
+  },
+  {
+    name: "Plesiosaurus",
+    terrains: [
+      { name: "Salt Water", percentage: 85 },
+      { name: "Sand", percentage: 10 },
+      { name: "Brown Stone", percentage: 5 }
+    ]
+  },
+  {
+    name: "Stegosaurus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 70 },
+      { name: "Grass", percentage: 10 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Ankylosaurus",
+    terrains: [
+      { name: "Savannah Grass", percentage: 80 },
+      { name: "Fresh Water", percentage: 10 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 5 }
+    ]
+  },
+  {
+    name: "Deinosuchus",
+    terrains: [
+      { name: "Salt Water", percentage: 70 },
+      { name: "Brown Stone", percentage: 20 },
+      { name: "Dirt", percentage: 10 }
+    ]
+  },
+  {
+    name: "Gallimimus",
+    terrains: [
+      { name: "Coniferous Floor", percentage: 80 },
+      { name: "Grass", percentage: 10 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Iguanodon",
+    terrains: [
+      { name: "Deciduous Floor", percentage: 75 },
+      { name: "Grass", percentage: 15 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Triceratops",
+    terrains: [
+      { name: "Savannah Grass", percentage: 75 },
+      { name: "Dirt", percentage: 15 },
+      { name: "Sand", percentage: 5 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Arctic Wolf",
+    terrains: [
+      { name: "Grey Stone", percentage: 15 },
+      { name: "Snow", percentage: 80 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Emperor Penguin",
+    terrains: [
+      { name: "Snow", percentage: 40 },
+      { name: "Salt Water", percentage: 60 }
+    ]
+  },
+  {
+    name: "Magnet",
+    terrains: [
+      { name: "Snow", percentage: 50 },
+      { name: "Salt Water", percentage: 50 }
+    ]
+  },
+  {
+    name: "Polar Bear",
+    terrains: [
+      { name: "Snow", percentage: 50 },
+      { name: "Salt Water", percentage: 50 }
+    ]
+  },
+  {
+    name: "Reindeer",
+    terrains: [
+      { name: "Grass", percentage: 15 },
+      { name: "Grey Stone", percentage: 10 },
+      { name: "Snow", percentage: 75 }
+    ]
+  },
+  {
+    name: "Yeti",
+    terrains: [
+      { name: "Grey Stone", percentage: 10 },
+      { name: "Snow", percentage: 90 }
+    ]
+  },
+  {
+    name: "Coelphysis",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 85 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Herrerasaurus",
+    terrains: [
+      { name: "Grass", percentage: 10 },
+      { name: "Rainforest Floor", percentage: 80 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Plateosaurus",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Fresh Water", percentage: 10 },
+      { name: "Deciduous Floor", percentage: 85 }
+    ]
+  },
+  {
+    name: "Allosaurus",
+    terrains: [
+      { name: "Grass", percentage: 20 },
+      { name: "Rainforest Floor", percentage: 70 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Giraffe",
+    terrains: [
+      { name: "Savannah Grass", percentage: 83 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 2 }
+    ]
+  },
+  {
+    name: "Greater Flamingo",
+    terrains: [
+      { name: "Savannah Grass", percentage: 10 },
+      { name: "Sand", percentage: 10 },
+      { name: "Dirt", percentage: 20 },
+      { name: "Fresh Water", percentage: 60 }
+    ]
+  },
+  {
+    name: "Hippopotamus",
+    terrains: [
+      { name: "Savannah Grass", percentage: 15 },
+      { name: "Dirt", percentage: 15 },
+      { name: "Fresh Water", percentage: 70 }
+    ]
+  },
+  {
+    name: "Leopard",
+    terrains: [
+      { name: "Savannah Grass", percentage: 70 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 20 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Olive Baboon",
+    terrains: [
+      { name: "Savannah Grass", percentage: 90 },
+      { name: "Dirt", percentage: 10 }
+    ]
+  },
+  {
+    name: "Ostrich",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Savannah Grass", percentage: 90 },
+      { name: "Dirt", percentage: 5 }
+    ]
+  },
+  {
+    name: "Plains Zebra",
+    terrains: [
+      { name: "Savannah Grass", percentage: 95 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Red Kangaroo",
+    terrains: [
+      { name: "Savannah Grass", percentage: 60 },
+      { name: "Sand", percentage: 8 },
+      { name: "Dirt", percentage: 30 },
+      { name: "Fresh Water", percentage: 2 }
+    ]
+  },
+  {
+    name: "Spotted Hyena",
+    terrains: [
+      { name: "Savannah Grass", percentage: 75 },
+      { name: "Sand", percentage: 10 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Thomson's Gazelle",
+    terrains: [
+      { name: "Savannah Grass", percentage: 95 },
+      { name: "Grey Stone", percentage: 5 }
+    ]
+  },
+  {
+    name: "Saltwater Crocodile",
+    terrains: [
+      { name: "Dirt", percentage: 20 },
+      { name: "Salt Water", percentage: 80 }
+    ]
+  },
+  {
+    name: "African Buffalo",
+    terrains: [
+      { name: "Savannah Grass", percentage: 80 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "African Elephant",
+    terrains: [
+      { name: "Savannah Grass", percentage: 65 },
+      { name: "Sand", percentage: 10 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 15 }
+    ]
+  },
+  {
+    name: "African Lion",
+    terrains: [
+      { name: "Savannah Grass", percentage: 80 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "African Warthog",
+    terrains: [
+      { name: "Savannah Grass", percentage: 20 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 70 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "African Wild Dog",
+    terrains: [
+      { name: "Savannah Grass", percentage: 75 },
+      { name: "Sand", percentage: 10 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Black Rhinoceros",
+    terrains: [
+      { name: "Savannah Grass", percentage: 85 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Blackbuck",
+    terrains: [
+      { name: "Savannah Grass", percentage: 95 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Cheetah",
+    terrains: [
+      { name: "Savannah Grass", percentage: 90 },
+      { name: "Dirt", percentage: 10 }
+    ]
+  },
+  {
+    name: "Common Wildebeest",
+    terrains: [
+      { name: "Savannah Grass", percentage: 80 },
+      { name: "Sand", percentage: 5 },
+      { name: "Dirt", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Asian Black Bear",
+    terrains: [
+      { name: "Grey Stone", percentage: 25 },
+      { name: "Snow", percentage: 25 },
+      { name: "Coniferous Floor", percentage: 50 }
+    ]
+  },
+  {
+    name: "Giant Panda",
+    terrains: [
+      { name: "Grass", percentage: 8 },
+      { name: "Grey Stone", percentage: 5 },
+      { name: "Snow", percentage: 5 },
+      { name: "Fresh Water", percentage: 2 },
+      { name: "Coniferous Floor", percentage: 80 }
+    ]
+  },
+  {
+    name: "Ibex",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Brown Stone", percentage: 20 },
+      { name: "Grey Stone", percentage: 70 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Llama",
+    terrains: [
+      { name: "Dirt", percentage: 10 },
+      { name: "Grey Stone", percentage: 75 },
+      { name: "Snow", percentage: 10 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Markhor",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Grey Stone", percentage: 60 },
+      { name: "Snow", percentage: 30 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Mountain Lion",
+    terrains: [
+      { name: "Grass", percentage: 5 },
+      { name: "Brown Stone", percentage: 70 },
+      { name: "Grey Stone", percentage: 20 },
+      { name: "Fresh Water", percentage: 5 }
+    ]
+  },
+  {
+    name: "Snow Leopard",
+    terrains: [
+      { name: "Brown Stone", percentage: 10 },
+      { name: "Grey Stone", percentage: 30 },
+      { name: "Snow", percentage: 60 }
+    ]
+  },
+  {
+    name: "Asian Elephant",
+    terrains: [
+      { name: "Grass", percentage: 20 },
+      { name: "Rainforest Floor", percentage: 60 },
+      { name: "Fresh Water", percentage: 20 }
+    ]
+  },
+  {
+    name: "Bengal Tiger",
+    terrains: [
+      { name: "Grass", percentage: 15 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 70 },
+      { name: "Fresh Water", percentage: 10 }
+    ]
+  },
+  {
+    name: "Black Leopard",
+    terrains: [
+      { name: "Grass", percentage: 10 },
+      { name: "Dirt", percentage: 5 },
+      { name: "Rainforest Floor", percentage: 85 }
+    ]
+  },
 ]
 
 animals_with_terrains.each do |animal_data|
