16afb63030afcd3fdd3b17094ff84a02d870d295
Ellen Uttley <ellenuttley@gmail.com>
2024-10-05 16:14:05 +0100

    added tests - should not save exhibit size without size, size2, and capacity. all passing

--- test/models/exhibit_size_test.rb
+++ test/models/exhibit_size_test.rb
@@ -6,7 +6,21 @@ class ExhibitSizeTest < ActiveSupport::TestCase
     @exhibit_size = create(:exhibit_size, animal: @animal)
   end
 
-  
+  test "should not save exhibit size without size1" do
+    @exhibit_size.size1 = nil
+    assert_not @exhibit_size.save, "FAIL : Saved the exhibit size without size1"
+  end
+
+  test "should not save exhibit size without size2" do
+    @exhibit_size.size2 = nil
+    assert_not @exhibit_size.save, "FAIL : Saved the exhibit size without size2"
+  end
+
+  test "should not save exhibit size without capacity" do
+    @exhibit_size.capacity = nil
+    assert_not @exhibit_size.save, "FAIL : Saved the exhibit size without capacity"
+  end
+
 
 
 end
