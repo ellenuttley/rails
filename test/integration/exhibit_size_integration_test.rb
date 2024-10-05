587945499f33f4cb1e733611fe529e5444b8f9ff
Ellen Uttley <ellenuttley@gmail.com>
2024-10-05 21:14:41 +0100

    added test for updating exhibit size. passes

--- test/integration/exhibit_size_integration_test.rb
+++ test/integration/exhibit_size_integration_test.rb
@@ -10,4 +10,13 @@ class AnimalTerrainIntegrationTest < ActionDispatch::IntegrationTest
       assert @exhibit_size.valid?
       assert_equal @animal, @exhibit_size.animal
     end
+
+    test "updating exhibit size of an animal" do
+      @animal.exhibit_size.update(size1: 25, size2: 20, capacity: 6)
+  
+      @animal.reload
+      assert_equal 25, @animal.exhibit_size.size1
+      assert_equal 20, @animal.exhibit_size.size2
+      assert_equal 6, @animal.exhibit_size.capacity
+    end
 end
