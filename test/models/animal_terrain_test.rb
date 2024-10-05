c207edcb9ba9167f63ee76648ef0c9265aaabff0
Ellen Uttley <ellenuttley@gmail.com>
2024-10-05 21:56:52 +0100

    removed test added in error

--- test/models/animal_terrain_test.rb
+++ test/models/animal_terrain_test.rb
@@ -11,12 +11,6 @@ class AnimalTerrainTest < ActiveSupport::TestCase
     assert animal_terrain.valid?
   end
 
-  test "must have animal" do
-    animal_terrain = build(:animal_terrain, animal: nil, terrain: @terrain, percentage: 50)
-    assert_not animal_terrain.valid?
-    assert_includes animal_terrain.errors[:animal], "must exist"
-  end
-
   test "must have terrain" do
     animal_terrain = build(:animal_terrain, animal: @animal, terrain: nil, percentage: 50)
     assert_not animal_terrain.valid?
