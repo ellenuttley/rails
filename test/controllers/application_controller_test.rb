83400d6c3ce3c54062078539ae7b9d2cd20c08a7
Ellen Uttley <ellenuttley@gmail.com>
2024-10-06 17:03:22 +0100

    added rails api, and adjusted test. still failing

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test "should return JSON content type for API requests" do
-    get '/api/some_endpoint'
+    get '/api/test'
     assert_equal "application/json", @response.content_type
   end
 end
