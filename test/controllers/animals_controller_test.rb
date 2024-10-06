29787910f569b9e714ad6088a8e3a80b1e46b18a
Ellen Uttley <ellenuttley@gmail.com>
2024-10-06 17:54:10 +0100

    Revert "edited the test to include a check for the json. passing"
    This reverts commit 42b574c60d35d9c79bf8dbaf6a0b8d7eb3466107.

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -1,10 +1,9 @@
-require 'test_helper'
+require "test_helper"
 
 class AnimalsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
     assert_response :success
-    assert_equal "application/json", @response.media_type
     assert_not_nil @controller.instance_variable_get('@animals')
   end
 end
